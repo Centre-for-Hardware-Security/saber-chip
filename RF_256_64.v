6f77ed6ddfd2aab69454f70f6eda7f76d47bb9dc
S. Pagliarini <50336652+snpagliarini@users.noreply.github.com>
2021-09-17 16:13:25 +0300

    Create RF_256_64.v

---
+++ RF_256_64.v
@@ -0,0 +1,16 @@
+module RF_256_64 ( CLK, CEB, WEB, A, D, BWEB, Q );
+
+parameter numWord = 256;
+parameter numRow = 64;
+parameter numCM = 4;
+parameter numBit = 64;
+parameter numWordAddr = 8;
+parameter numRowAddr = 6;
+parameter numCMAddr = 2;
+	
+// this is a proprietary model and cannot be shared openly. we make the parameters available above for reference.
+
+endmodule
+`endcelldefine
+
+
